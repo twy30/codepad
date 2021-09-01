b8dd8f8d69f91a4f4d59216b1c67474997683362
TW Yang <twy@30abysses.com>
2021-08-31 21:18:17 -0400

    Double-quote Git configs.

--- Git/set-configs.bash
+++ Git/set-configs.bash
@@ -1,6 +1,6 @@
 git config --global core.editor 'code --wait'
 git config --global diff.tool vscode
 git config --global difftool.prompt false
-git config --global difftool.vscode.cmd 'code --wait --diff $LOCAL $REMOTE'
+git config --global difftool.vscode.cmd 'code --wait --diff "$LOCAL" "$REMOTE"'
 git config --global merge.tool vscode
-git config --global mergetool.vscode.cmd 'code --wait $MERGED'
+git config --global mergetool.vscode.cmd 'code --wait "$MERGED"'
